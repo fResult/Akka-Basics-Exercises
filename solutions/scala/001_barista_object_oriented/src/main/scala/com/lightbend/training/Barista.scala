b679016922bd80920d08941f40c1d0ec5ca4b11b
fResult <styxmaz@gmail.com>
2025-03-18 13:44:08 +0700

    1st commit - Setup project.

---
+++ solutions/scala/001_barista_object_oriented/src/main/scala/com/lightbend/training/Barista.scala
@@ -0,0 +1,32 @@
+package com.lightbend.training
+
+import scala.collection._
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl._
+
+object Barista {
+
+  def apply(): Behavior[OrderCoffee] =
+    Behaviors.setup(newBaristaBehavior(_))
+
+  def printOrders(orders: Set[(String, Coffee)]): String = {
+    val formattedOrders = orders.map(order => s"${order._1}->${order._2}")
+        .reduce((acc, s) => acc + "," + s)
+    s"[$formattedOrders]"
+  }
+
+  final case class OrderCoffee(whom: String, coffee: Coffee)
+
+  class BaristaBehavior(context: ActorContext[OrderCoffee]) extends AbstractBehavior[OrderCoffee](context) {
+    private val orders: mutable.Map[String, Coffee] = mutable.Map()
+
+    override def onMessage(message: OrderCoffee): Behavior[OrderCoffee] = {
+      message match {
+        case OrderCoffee(whom, coffee) =>
+          orders.put(whom, coffee)
+          context.log.info(s"Orders:${printOrders(orders.toSet)}")
+          this
+      }
+    }
+  }
+}
