814d22f4f5e8c50fdb4eda505fccd3ed4c408c3c
fResult <styxmaz@gmail.com>
2025-03-18 17:33:30 +0700

    Implement the `BaristaBehavior` as Actor with its message passing.

--- exercises/scala/001_barista_object_oriented/src/main/scala/com/lightbend/training/Barista.scala
+++ exercises/scala/001_barista_object_oriented/src/main/scala/com/lightbend/training/Barista.scala
@@ -1,11 +1,13 @@
 package com.lightbend.training
 
-import scala.collection._
 import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
+
+import scala.collection.*
 
 object Barista {
 
-  def apply(): Behavior[OrderCoffee] = ???
+  def apply(): Behavior[OrderCoffee] = Behaviors.setup[OrderCoffee](BaristaBehavior(_))
 
   def printOrders(orders: Set[(String, Coffee)]): String = {
     val formattedOrders = orders.map(order => s"${order._1}->${order._2}")
@@ -14,4 +16,18 @@ object Barista {
   }
 
   final case class OrderCoffee(whom: String, coffee: Coffee)
+
+  private class BaristaBehavior(context: ActorContext[OrderCoffee])
+      extends AbstractBehavior[OrderCoffee](context) {
+
+    private val orders: mutable.Map[String, Coffee] = mutable.Map()
+
+    override def onMessage(message: OrderCoffee): Behavior[OrderCoffee] =
+      message match
+        case OrderCoffee(whom, coffee) =>
+          orders += (whom -> coffee)
+          context.log.info(s"Orders: ${printOrders(orders.toSet)}")
+
+      this
+  }
 }
